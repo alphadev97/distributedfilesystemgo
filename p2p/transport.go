d1f25dfd66cfe7a03083415089ffc6189ddcc24f
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-08-02 04:59:14 +0500

    transport

---
+++ p2p/transport.go
@@ -0,0 +1,13 @@
+package p2p
+
+// Peer is an interface that respresents the remote node
+type Peer interface {
+}
+
+// Transport is anything that handles the communication
+// between the nodes in the network. this can be of the
+// from (TCP, UDP, websockets, ......)
+
+type Transport interface {
+	ListenAndAccept() error
+}
