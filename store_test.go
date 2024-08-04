69417a3803a1d5c541eaa4d3bc81e6572cf1e4f0
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-08-04 19:28:56 +0500

    path

--- store_test.go
+++ store_test.go
@@ -2,19 +2,27 @@ package main
 
 import (
 	"bytes"
-	"fmt"
 	"testing"
 )
 
 func TestPathTransformFunc(t *testing.T) {
 	key := "mamasbestpicture"
-	pathname := CASPathTransformFunc(key)
-	fmt.Println(pathname)
+	pathKey := CASPathTransformFunc(key)
+	expectedOriginalKey := "4018a9a4e2c66eae1c409b5e46a761001d50f15f"
+	expectedPathName := "4018a/9a4e2/c66ea/e1c40/9b5e4/6a761/001d5/0f15f"
+
+	if pathKey.PathName != expectedPathName {
+		t.Errorf("have %s want %s", pathKey.PathName, expectedPathName)
+	}
+
+	if pathKey.Original != expectedPathName {
+		t.Errorf("have %s want %s", pathKey.Original, expectedOriginalKey)
+	}
 }
 
 func TestStore(t *testing.T) {
 	opts := StoreOpts{
-		PathTransformFunc: DefaultPathTransformFunc,
+		PathTransformFunc: CASPathTransformFunc,
 	}
 	s := NewStore(opts)
 
