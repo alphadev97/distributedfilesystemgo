f922db4d630890f9fe366e8b394344df417c2e3b
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-08-06 05:45:27 +0500

    delete test

--- store_test.go
+++ store_test.go
@@ -22,52 +22,55 @@ func TestPathTransformFunc(t *testing.T) {
 	}
 }
 
-func TestStoreDeletKey(t *testing.T) {
+func TestStore(t *testing.T) {
 
-	opts := StoreOpts{
-		PathTransformFunc: CASPathTransformFunc,
-	}
-	s := NewStore(opts)
-	key := "momsspecials"
-	data := []byte("some jpg bytes")
+	s := newStore()
+	defer teardown(t, s)
 
-	if err := s.writeStream(key, bytes.NewReader(data)); err != nil {
-		t.Error(err)
+	for i := 0; i < 20; i++ {
+		key := fmt.Sprintf("foo_%d", i)
+		data := []byte("some jpg bytes")
+
+		if err := s.writeStream(key, bytes.NewReader(data)); err != nil {
+			t.Error(err)
+		}
+
+		if ok := s.Has(key); !ok {
+			t.Errorf("exptected to have key %s", key)
+		}
+
+		r, err := s.Read(key)
+		if err != nil {
+			t.Error(err)
+		}
+
+		b, _ := ioutil.ReadAll(r)
+		if string(b) != string(data) {
+			t.Errorf("want %s have %s", data, b)
+		}
+
+		fmt.Println(string(b))
+
+		if err := s.Delete(key); err != nil {
+			t.Error(err)
+		}
+
+		if ok := s.Has(key); ok {
+			t.Errorf("exptected to NOT have key %s", key)
+		}
 	}
 
-	if err := s.Delete(key); err != nil {
-		t.Error(err)
-	}
 }
 
-func TestStore(t *testing.T) {
+func newStore() *Store {
 	opts := StoreOpts{
 		PathTransformFunc: CASPathTransformFunc,
 	}
-	s := NewStore(opts)
-	key := "momsspecials"
-	data := []byte("some jpg bytes")
+	return NewStore(opts)
+}
 
-	if err := s.writeStream(key, bytes.NewReader(data)); err != nil {
+func teardown(t *testing.T, s *Store) {
+	if err := s.Clear(); err != nil {
 		t.Error(err)
 	}
-
-	if ok := s.Has(key); !ok {
-		t.Errorf("exptected to have key %s", key)
-	}
-
-	r, err := s.Read(key)
-	if err != nil {
-		t.Error(err)
-	}
-
-	b, _ := ioutil.ReadAll(r)
-	if string(b) != string(data) {
-		t.Errorf("want %s have %s", data, b)
-	}
-
-	fmt.Println(string(b))
-
-	s.Delete(key)
-
 }
